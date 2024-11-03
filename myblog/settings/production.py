b42e9c83bbc94663805aa7ae73c508e34db5e345
Clément Zentz <clzt94@gmail.com>
2024-11-03 17:38:36 +0100

    Prepare for first deployment

--- myblog/settings/production.py
+++ myblog/settings/production.py
@@ -8,13 +8,30 @@ ALLOWED_HOSTS = config(
 
 DEBUG = False
 
+# Activer le redirection HTTPS
+SECURE_SSL_REDIRECT = True
+
+# Configurer HSTS
+SECURE_HSTS_SECONDS = 3600  # 1 heure (vous pouvez augmenter cette valeur une fois que vous êtes sûr que tout fonctionne correctement)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# Autres paramètres de sécurité
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+CSP_DEFAULT_SRC = ("'self'",)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'NAME': config('PROD_DATABASE_NAME'),
+        'USER': config('PROD_DATABASE_USER'),
+        'PASSWORD': config('PROD_DATABASE_PASSWORD'),
+        'HOST': config('PROD_DATABASE_HOST'),
+        'PORT': config('PROD_DATABASE_PORT'),
     }
 }

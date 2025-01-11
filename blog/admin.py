cbbbb918e3b07fa18235df79fae5839a400dca6d
Clément Zentz <clzt94@outlook.fr>
2025-01-11 19:07:05 +0100

    improved admin UI for posts and category translation

--- blog/admin.py
+++ blog/admin.py
@@ -13,8 +13,25 @@ class PostTranslationInline(admin.TabularInline):
 # Définir une class d'administration pour le modèle Post
 # pour personnaliser l'interface admin du modèle Post
 class PostAdmin(admin.ModelAdmin):
-    # Ajouter les traductions en ligne
     inlines = [PostTranslationInline]
+    list_display = ('id', 'get_title', 'created_date', 'updated_date', 'approved')
+    list_filter = ('approved', 'created_date', 'updated_date')
+    search_fields = ('translations__title', 'translations__content')
+    actions = ['approve_posts', 'unapprove_posts']
+
+    def get_title(self, obj):
+        return obj.translations.first().title\
+            if obj.translations.exists()\
+            else 'No translation'
+    get_title.short_description = 'Title'
+
+    def approve_posts(self, request, queryset):
+        queryset.update(approved=True)
+    approve_posts.short_description = 'Approve selected posts'
+
+    def unapprove_posts(self, request, queryset):
+        queryset.update(approved=False)
+    unapprove_posts.short_description = 'Unapprove selected posts'
 
 class CategoryTranslationInline(admin.TabularInline):
     model = CategoryTranslation 
@@ -22,6 +39,14 @@ class CategoryTranslationInline(admin.TabularInline):
 
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [CategoryTranslationInline]
+    list_display = ('id', 'get_name')
+    search_fields = ('translations__name',)
+
+    def get_name(self, obj):
+        return obj.translations.first().name\
+            if obj.translations.exists()\
+            else 'No translation'
+    get_name.short_description = 'Name'
 
 # Register your models here.
 admin.site.register(Category, CategoryAdmin)
@@ -31,5 +56,8 @@ admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
 # Enregistre le modele PostTranslation
 # pour qu'il soit disponible dans l'interface d'admin
-# admin.site.register(PostTranslation)
+admin.site.register(PostTranslation)
 admin.site.register(Comment)
+
+# TODO afficher le titre des posts dans 
+# l'interface admin en fonction de la langue.
